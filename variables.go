1240b5e5ae131975893d6890eed6203fbdb78b07
Arretados Labs <arretadolabs@gmail.com>
2024-07-15 16:47:34 -0300

    Inserting script with manipulation, interpolation and data type in variables

--- variables.go
+++ variables.go
@@ -4,8 +4,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("============== Section of variables ==================")
 
-	fmt.Print("======================================================")
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Print("Printing type of data store in variables\n")
+
+	//Using %T for discover of type data store in variable
+	fmt.Printf("ConferenceTickets is %T, remainingTickets %T and conferenceName %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Println("============= Printing values with on format interpolation =============")
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName) //Concept of interpolation and explict new line "\n"
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	fmt.Println("================= manipulation variables =================")
+
+	//declaring variables
+	var userName string
+	var userTickets int
+
+	userName = "Francisco"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
